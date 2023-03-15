577b1815dbae6aad9d6e93e1d41748abb757949c
a483210 <a483210@163.com>
2023-03-15 18:59:33 +0800

    修改apache rocketmq部署配置

--- sample/k8s/directory.sh
+++ sample/k8s/directory.sh
@@ -1,19 +1,28 @@
+#创建命名空间
+kubectl create namespace rocketmq
+
 #创建文件夹
 
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-1
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-1a
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-1as
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-1as2
-mkdir /Volumes/Develop/rockate-cluster/temp/store-1
-mkdir /Volumes/Develop/rockate-cluster/temp/store-1a
-mkdir /Volumes/Develop/rockate-cluster/temp/store-1as
-mkdir /Volumes/Develop/rockate-cluster/temp/store-1as2
+mkdir /data/rocketmq/cluster
+chmod -R 777 /data/rocketmq/cluster
 
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-2
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-2b
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-2bs
-mkdir /Volumes/Develop/rockate-cluster/temp/logs-2bs2
-mkdir /Volumes/Develop/rockate-cluster/temp/store-2
-mkdir /Volumes/Develop/rockate-cluster/temp/store-2b
-mkdir /Volumes/Develop/rockate-cluster/temp/store-2bs
-mkdir /Volumes/Develop/rockate-cluster/temp/store-2bs2
+mkdir /data/rocketmq/cluster/config-console
+mkdir /data/rocketmq/cluster/logs-console
+
+mkdir /data/rocketmq/cluster/logs-1
+mkdir /data/rocketmq/cluster/logs-1a
+mkdir /data/rocketmq/cluster/logs-1as
+mkdir /data/rocketmq/cluster/logs-1as2
+mkdir /data/rocketmq/cluster/store-1
+mkdir /data/rocketmq/cluster/store-1a
+mkdir /data/rocketmq/cluster/store-1as
+mkdir /data/rocketmq/cluster/store-1as2
+
+mkdir /data/rocketmq/cluster/logs-2
+mkdir /data/rocketmq/cluster/logs-2b
+mkdir /data/rocketmq/cluster/logs-2bs
+mkdir /data/rocketmq/cluster/logs-2bs2
+mkdir /data/rocketmq/cluster/store-2
+mkdir /data/rocketmq/cluster/store-2b
+mkdir /data/rocketmq/cluster/store-2bs
+mkdir /data/rocketmq/cluster/store-2bs2
